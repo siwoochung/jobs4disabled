a24bcaaae598bf3aa797554875f7ccdd1319aea4
siwoochung <128882123+siwoochung@users.noreply.github.com>
2023-04-01 15:57:53 +0900

    s

--- recommendation.py
+++ recommendation.py
@@ -10,11 +10,16 @@ def pick_jobs(n):
 	location = random_data["사업장 주소"].tolist()
 	company_type = random_data["기업형태"].tolist()
 	job_class = random_data["Classification"].tolist()
+	pay_type = random_data["임금형태"].tolist()
+	required_degree = random_data["요구학력"].tolist()
+	required_work = random_data["요구경력"].tolist()
+
 
 	final_lst=[]
 	for i in range(NUMS):
 		dic = dict()  #{}
 		dic["이름"]=name[i]
+
 		dic["고용형태"]=hire_type[i]
 		if hire_type[i]==0:
 			dic["고용형태"] = "계약직"
@@ -22,7 +27,9 @@ def pick_jobs(n):
 			dic["고용형태"] = "상용직"
 		else:
 			dic["고용형태"] = "시간제"
+
 		dic["사업장 주소"]=location[i]
+
 		dic["기업형태"]=company_type[i]
 		if company_type[i]==0:
 			dic["기업형태"]="개인"
@@ -34,8 +41,31 @@ def pick_jobs(n):
 			dic["기업형태"]="공사,공공"
 		elif company_type[i]==4:
 			dic["기업형태"]="대기업"
+
 		dic["분류"]=job_class[i]
-		final_lst.append(dic)
+
+		dic["임금형태"]=pay_type[i]
+		if pay_type[i]==0:
+			dic["임금형태"]="시급"
+		elif pay_type[i]==1:
+			dic["임금형태"]="일급"
+		elif pay_type[i]==2:
+			dic["임금형태"]="월급"
+		else:
+			dic["임금형태"]="연봉"
+
+		dic["요구학력"]=required_degree[i]
+		if required_degree[i]==0:
+			dic["요구학력"]="학무관"
+		elif required_degree[i]==1:
+			dic["요구학력"]="고졸"
+		elif required_degree[i]==2:
+			dic["요구학력"]="초대졸"
+		else:
+			dic["요구학력"]="대졸"
+
+		dic["요구경력"]=required_work[i]
+	
 	return final_lst
 
 NUMS = 10
